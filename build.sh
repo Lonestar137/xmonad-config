43bc79c561c49ea3580781e77260d529944e497f
Garrett Jones <garretcj2010@hotmail.com>
2023-09-03 00:53:55 +0000

    initial config

---
+++ build.sh
@@ -0,0 +1,3 @@
+cabal install --package-env=$HOME/.config/xmonad --lib base xmonad-contrib xmonad
+cabal install --package-env=$HOME/.config/xmonad --lib base xmonad xmonad-contrib
+cabal install --package-env=$HOME/.config/xmonad xmonad
